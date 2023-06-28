39880454c66e01c06056dbfbc05ceb454ae1295f
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-06-28 00:25:52 -0700

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,8 +10,12 @@ function App() {
   return (
     <>
       <div>
-        <Header />
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Header />} />
+            <Route path="/home" element={<Hero />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
